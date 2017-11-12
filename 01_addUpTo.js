367c50ba4905f7e90e28a2446ed91422b44b11c8
Geoff Gladden <ggladden@gmail.com>
2017-11-12 12:42:19 -0800

    Initial commit

---
+++ 01_addUpTo.js
@@ -0,0 +1,34 @@
+function addUpTo(num) {
+  if (typeof num !== 'number') return NaN;
+  let result = 0;
+  
+  for (let i = 0; i <=num; i++) {
+    result += i;
+  }
+  return result;
+}
+
+// Tests
+
+const assert = require('assert');
+
+describe('addUpTo()', function() {
+  it('sum all numbers from 0 up to the argument', function() {
+    assert.equal(addUpTo(3), 6);
+    assert.equal(addUpTo(2), 3);
+  });
+
+  it('should sum only whole numbers', function() {
+    assert.equal(addUpTo(5.9), 15);
+  });
+
+  it('should return 0 if the argument is less than 1', function() {
+    assert.equal(addUpTo(-1), 0);
+  });
+
+  it('should return NaN if the argument is not a number', function() {
+    assert.ok(isNaN(addUpTo('string')));
+    assert.ok(isNaN(addUpTo(null)));
+    assert.ok(isNaN(addUpTo([])));
+  })
+});
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # javascript-warmups
+
+A series of warmup problems in JavaScript. Each file contains a function and tests for that function. Run the tests via the command line with mocha.
+
+Example:
+
+    mocha 01_addUpTo.js
